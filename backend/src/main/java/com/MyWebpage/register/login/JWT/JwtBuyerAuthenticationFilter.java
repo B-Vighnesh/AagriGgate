b7a61647824d48a463156ae91a655563f23f456f
B Vighnesh Kumar <vighneshsheregar2004@gmail.com>
2026-02-23 11:44:34 +0530

    backend at the point which is not suitable for industries

--- backend/src/main/java/com/MyWebpage/register/login/JWT/JwtBuyerAuthenticationFilter.java
+++ backend/src/main/java/com/MyWebpage/register/login/JWT/JwtBuyerAuthenticationFilter.java
@@ -8,7 +8,7 @@
 //import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 //import org.springframework.web.filter.OncePerRequestFilter;
-//
+
 //import jakarta.servlet.FilterChain;
 //import jakarta.servlet.ServletException;
 //import jakarta.servlet.http.HttpServletRequest;
