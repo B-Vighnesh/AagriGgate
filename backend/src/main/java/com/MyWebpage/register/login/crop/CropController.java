4acee1d56eba18e546b82d2a17c0f9dcf862be25
B Vighnesh Kumar <vighneshsheregar2004@gmail.com>
2026-04-02 14:14:49 +0530

    environment variables added

--- backend/src/main/java/com/MyWebpage/register/login/crop/CropController.java
+++ backend/src/main/java/com/MyWebpage/register/login/crop/CropController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/crops")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 public class CropController {
 
     private final CropService cropService;
