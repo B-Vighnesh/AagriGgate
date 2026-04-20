aaf385927450677a69ff2494fc66d1743bc392a5
B Vighnesh Kumar <vighneshsheregar2004@gmail.com>
2026-04-20 12:31:18 +0530

    Notification UI is properly modiifed

--- frontend/src/components/Notifications.jsx
+++ frontend/src/components/Notifications.jsx
@@ -17,6 +17,8 @@ const TIME_FORMATTER = new Intl.DateTimeFormat('en-GB', {
   day: '2-digit',
   month: 'short',
   year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
 });
 
 const syncNotificationCount = (count) => {
